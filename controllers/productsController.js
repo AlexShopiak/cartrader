141ce13a40fec1aa2dfb0255ef78f9287dd81dd3
Alex Shopiak <alshop2004@gmail.com>
2023-10-30 13:32:17 +0000

    Refactor product controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,10 +3,7 @@ const Product = require('../models/Product');
 getAllProducts = async (req, res) => {
     try {
         const products = await Product.find(); 
-        if (!products) {
-            res.render('products', { products: [] });
-        }
-        res.render('products', { products: Array.isArray(products) ? products : [products] });
+        res.render('products', { products: products });
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
@@ -14,17 +11,17 @@ getAllProducts = async (req, res) => {
 };
 
 getProductByName = async (req, res) => {
-    const productName = req.query.name;
-    if (!productName) {
+    const name = req.query.name;
+    if (!name) {
         return getAllProducts(req, res);
     }
     try {
-        const products = await Product.findOne({name:productName});
-        if (!products) {
-            return res.render('products_empty', {message: `No products with the name: ${productName}`});
+        const product = await Product.findOne({name:name});
+        if (product == null) {
+            res.render('products_empty', { message: `We have no: ${name}`});
+        } else {
+            res.render('products', { products: [product] });
         }
-        console.log(products)
-        res.render('products', { products: Array.isArray(products) ? products : [products] });
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');

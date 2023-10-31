5a6b691831b3aca772a8ea325cfc8b13663d6b77
Alex Shopiak <alshop2004@gmail.com>
2023-10-31 13:12:42 +0000

    Upgrade user info box

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,9 @@
 exports.profile = (req, res) => {
     if (req.session.user) {
-        res.render('profile/profile_user', {msg: req.session.user.name});
+        res.render('profile/profile_user', {
+            name: req.session.user.name, 
+            items: 5 //stub
+        });
     } else {
         res.redirect('/auth/login')
     }

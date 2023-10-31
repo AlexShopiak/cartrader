89a4a4c8b3c4123d97fe6436d8b82f06f247b9d8
Alex Shopiak <alshop2004@gmail.com>
2023-10-31 00:54:15 +0000

    Import auth libs & Create new auth route

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const path = require('path');
-const pug = require('pug');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -11,17 +12,26 @@ mongoose.connect('mongodb+srv://alex:0000@sandboxcluster.wh0wned.mongodb.net/?re
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(session({
+    secret: '0000',
+    resave: false,
+    saveUninitialized: true,
+}));
+
 const mainRoutes = require('./routes/mainRoutes');
 const productsRoutes = require('./routes/productsRoutes');
 const profileRoutes = require('./routes/profileRoutes');
+const authRoutes = require('./routes/authRoutes');
 
-app.use('/', mainRoutes);
+app.use('/main', mainRoutes);
 app.use('/products', productsRoutes);
 app.use('/profile', profileRoutes);
+app.use('/auth', authRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log('Listen on:', port);
-    console.log(`http://localhost:${port}/`);
+    console.log(`http://localhost:${port}/main`);
 });

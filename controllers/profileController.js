b170ea1d7c63ba09544616972ec13738cad56497
Alex Shopiak <alshop2004@gmail.com>
2023-10-31 12:41:03 +0000

    Remove guest profile page

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,6 +2,6 @@ exports.profile = (req, res) => {
     if (req.session.user) {
         res.render('profile/profile_user', {msg: req.session.user.name});
     } else {
-        res.render('profile/profile_guest', {});
+        res.redirect('/auth/login')
     }
 };

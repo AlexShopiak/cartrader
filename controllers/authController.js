6c46bcb617d1890cab7e5b5fecdc4df62800df9b
Alex Shopiak <alshop2004@gmail.com>
2023-10-31 00:56:37 +0000

    Create authController

---
+++ controllers/authController.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+exports.getLogin = (req, res) => {
+    res.render('auth_login', {});
+}; 
+
+exports.postLogin = (req, res) => {
+    if(!req.body.name || !req.body.password){
+        res.status("400");
+        res.send("Invalid details!");
+    } else {
+        if('Alex' === req.body.name && '1234' === req.body.password){
+            const newUser = {name: req.body.name, password: req.body.password};
+            req.session.user = newUser;
+            res.redirect('/profile');
+        }
+    }
+}; 
+
+exports.getSignup = (req, res) => {
+    res.render('auth_signup', {});
+};
+
+exports.postSignup = (req, res) => {
+    res.render('auth_signup', {});//todo
+}; 
+
+exports.getLogout = (req, res) => {
+    req.session.destroy(() => {
+        console.log("user logged out.")
+    });
+    res.redirect('/profile');
+};

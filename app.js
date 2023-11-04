9d6ec2d92bc35dd3c88e0ed5117af550a74e4e59
Alex Shopiak <alshop2004@gmail.com>
2023-11-04 15:32:28 +0000

    Refactor app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -7,18 +9,22 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-mongoose.connect('mongodb+srv://alex:0000@sandboxcluster.wh0wned.mongodb.net/?retryWrites=true&w=majority');
+//DB connecting
+const uri = 'mongodb+srv://alex:0000@sandboxcluster.wh0wned.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(uri);
 
+//Middlewares
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret: '0000',
     resave: false,
     saveUninitialized: true,
 }));
 
+//Routes
 const mainRoutes = require('./routes/mainRoutes');
 const productsRoutes = require('./routes/productsRoutes');
 const profileRoutes = require('./routes/profileRoutes');
@@ -29,8 +35,10 @@ app.use('/products', productsRoutes);
 app.use('/profile', profileRoutes);
 app.use('/auth', authRoutes);
 
+//Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Running server
 app.listen(port, () => {
     console.log('Listen on:', port);
     console.log(`http://localhost:${port}/main`);

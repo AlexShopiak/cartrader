27bdf6e947cd23a0b9f5c72ac3ab6a7c9706cb78
Alex Shopiak <alshop2004@gmail.com>
2024-02-18 15:47:27 +0000

    Refactor entry point and add Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './styles.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Profile from './pages/Profile'
+import NoPage from './pages/NoPage'
+
+import './main.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/products',
+    element: <Products />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  },
+  {
+    path: '*',
+    element: <NoPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);

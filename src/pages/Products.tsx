2419139481483fd208a4b8e4d899699118e82427
Alex Shopiak <alshop2004@gmail.com>
2024-03-03 13:57:14 +0000

    Add ProductsContainer component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,12 +3,17 @@ import React, {useState} from 'react';
 
 import './Products.css'
 
-const ProductsSearchBar = () => {
+interface Product {
+    name: string;
+    description: string;
+	owner: string;
+    price: number;
+}
+
+const ProductsSearchBar = ({ owners } : {owners: string[]}) => {
 	const [sorting, setSorting] = useState("");
 	const [owner, setOwner] = useState("");
 	const [name, setName] = useState("");
-
-	const owners = ["Alex", "Andrew", "Max"];//STUB
 
 	const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
 		setSorting(event.target.value);
@@ -23,6 +28,7 @@ const ProductsSearchBar = () => {
 	}
 
 	const handleFindClick = () => {
+		//? setName("");
 		console.log("click Find");//STUB
 	}
 
@@ -41,27 +47,52 @@ const ProductsSearchBar = () => {
 		</select>
 
 		<input className='name-input' 
-				type='text' 
-				placeholder='Find by name' 
-				value={name} 
-				onChange={handleNameChange}>
+			type='text' 
+			placeholder='Find by name' 
+			value={name} 
+			onChange={handleNameChange}>
 		</input>
 
-		<button className='search-button' onClick={handleFindClick}>Find</button>
+		<button className='blue-button' onClick={handleFindClick}>Find</button>
 	</div>)
 };
 
-const ProductsTable = () => {
-	return (<>
-		ProductsTable
-	</>)
+const ProductsContainer = ({ products } : {products: Product[]}) => {
+	const handleBuyClick = () => {
+		console.log("click Buy");//STUB
+	}
+
+	return (<div className='products-container'>
+		{products.length > 0 ? (
+			products.map((p) => 
+				<div className="product">
+					<h2>{p.name}</h2>
+					<p>{p.description}</p>
+					<p>Owner: {p.owner}</p>
+					<p>Price: {p.price} GBP</p>
+					<button className='blue-button' onClick={handleBuyClick}>Buy</button>
+				</div>
+			)
+		) : (
+			<h2>No products availible</h2>
+		)}
+		
+	</div>)
 };
 
 function Products() {
+	const ownersStub = ["Alex", "Andrew", "Max"];
+	const productsStub = [
+		{name:"Apple",  description:"Info1", owner:"alex", price:5},
+		{name:"Banana", description:"Info2", owner:"alex", price:10},
+		{name:"Potato", description:"Info3", owner:"alex", price:12},
+		{name:"Potato", description:"Info3", owner:"alex", price:12},
+	];
+
 	return (<>
 		<Navbar />
-		<ProductsSearchBar />
-		<ProductsTable />
+		<ProductsSearchBar owners={ownersStub}/>
+		<ProductsContainer products={productsStub}/>
 	</>)
 }
 

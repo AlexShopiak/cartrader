4d097b69984edc59ac0590e3a7ad4ac728779e47
Alex Shopiak <alshop2004@gmail.com>
2024-03-03 14:57:54 +0000

    Wrap frontend in a folder

---
+++ frontend/src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Profile from './pages/Profile'
+import NoPage from './pages/NoPage'
+
+import './main.css'
+
+const router = createBrowserRouter([
+  {path: '/',         element: <Home />},
+  {path: '/products', element: <Products />},
+  {path: '/profile',  element: <Profile />},
+  {path: '*',         element: <NoPage />},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
---
+++ frontend/src/pages/NoPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar'
+
+function NoPage() {
+  return (
+    <>
+      <Navbar />
+      <>NoPage</>
+    </>
+  )
+}
+
+export default NoPage

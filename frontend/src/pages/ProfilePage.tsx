249baa1c7bfb3dc33bbf93dfb22b42e50de0e6a0
Alex Shopiak <alshop2004@gmail.com>
2024-03-05 15:17:18 +0000

    Improve naming

---
+++ frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar'
+import './ProfilePage.css'
+
+function ProfilePage() {
+  return (
+    <>
+      <Navbar />
+      <>ProfilePage</>
+    </>
+  )
+}
+
+export default ProfilePage

23be9fc4366f147ca5da30b8cabe22ec10fdb92b
Alex Shopiak <alshop2004@gmail.com>
2024-04-17 00:33:52 +0000

    Implement About page

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Car Trader</h1>
+      <p className='mb-4 text-slate-700'>Car Trader is a leading car trader website that specializes in helping people buy or sell user or new cars. Our team is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>Our mission is to help our clients achieve their goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy or sell a vehicle, we are here to help you every step of the way.</p>
+      <p className='mb-4 text-slate-700'>Our team has a wealth of experience and knowledge in the car industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a car should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
+  )
+}
